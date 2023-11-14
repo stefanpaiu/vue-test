e9306966410ca58abbe63f1db0d7400e43a9a214
Stefan Paiu <stefan.paiu.05959@gmail.com>
2023-11-14 10:40:33 +0200

    Created Vue app

---
+++ app.js
@@ -0,0 +1,12 @@
+Vue.createApp({}).mount("#app");
+
+// Recommended Setup
+// import { createApp, ref } from 'vue'
+
+// createApp({
+//   setup() {
+//     return {
+//       count: ref(0)
+//     }
+//   }
+// }).mount('#app')
